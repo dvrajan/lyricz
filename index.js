9ef72e18b46b2bd8601b042d15824723a49007e9
Devarajan Narayanan <devarajn@thoughtworks.com>
2014-10-15 20:23:33 +0000

    Scaping a-songs

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-var http = require('http');
+var request = require('request');
+var cheerio = require("cheerio")
 
-http.get("http://www.paadalvarigal.com/", function(res){
-	console.log(res.data);
+request.get("http://www.paadalvarigal.com/a-songs", function(err, res, body){
+    $ = cheerio.load(body);
+    var songs = $("div#main p").find('a');
+    var hrefs = songs.map(function(i, elem){
+       return $(this).attr('href');
+    });
+    console.log(hrefs.length);
+    
 });
