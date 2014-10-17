f493c038c5238f456413ce72a9e4fff71efa8487
Deva <dvrajan12@gmail.com>
2014-10-17 15:00:23 +0530

    [Deva] Adding a static site

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var request = require('request');
-var cheerio = require("cheerio")
+var cheerio = require("cheerio");
+var _ = require("lodash");
 
-request.get("http://www.paadalvarigal.com/a-songs", function(err, res, body){
-    $ = cheerio.load(body);
-    var songs = $("div#main p").find('a');
-    var hrefs = songs.map(function(i, elem){
-       return $(this).attr('href');
-    });
-    console.log(hrefs.length);
-    
-});
+var seedUrls = ['http://www.paadalvarigal.com/'];
+
+function crawler (){
+	this.filterLinks	= function (body){					
+				$ = cheerio.load(body);
+				var links = $("body").find("a").map(function(i, element){
+					return $(this).attr('href');
+				});
+				console.log(links.length);				
+				this.crawl(links);
+		}
+
+		this.fetch = function (url, callback){
+				request.get(url, function(error, response, body){
+						callback(body);
+				});
+		}
+
+		this.crawl = function (urls){
+			var self = this;
+			_.forEach(urls, function(url){
+					 self.fetch(url, self.filterLinks);							
+			});
+		}	
+}
+
+
+new crawler().crawl(seedUrls);
