10e353eb8fa9e5dac4b35afb34a29975f14e58e1
Deva <dvrajan12@gmail.com>
2014-10-29 01:17:16 +0530

    removing objects

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ var request = require('request');
 var cheerio = require("cheerio");
 var _ = require("lodash");
 var store = require("./store");
-var memwatch = require('memwatch');
 var EventEmitter = require('events').EventEmitter;
 var emitter = new EventEmitter();
 var seedUrls = ['http://www.paadalvarigal.com'];
 
 
-function Crawler (baseUrl){
-	this.crawledUrls = [];
-	this.baseUrl = baseUrl;
-}
+
+crawledUrls = [];
+baseUrl = seedUrls[0];
 
 	
-Crawler.prototype.filterLinks = function (body){					
+function filterLinks(body){					
 		$ = cheerio.load(body);
 		var links = [];
 		$("body").find("a").each(function(i, element){
@@ -23,9 +21,8 @@ Crawler.prototype.filterLinks = function (body){
 		return links;
 };
 
-Crawler.prototype.fetch = function (url, callback){	  
-		var self = this;
-	  var newUrl = this.resolveUrl(url);	  
+function fetch(url, callback){	  		
+	  var newUrl = resolveUrl(url);	  
 	  if(newUrl == '')
 	  	return;
 		request.get(newUrl, function(error, response, body){				
@@ -33,7 +30,7 @@ Crawler.prototype.fetch = function (url, callback){
 		});
 };
 
-Crawler.prototype.parse = function(body){
+function parse(body){
 	$ = cheerio.load(body);
 	var lyrics = $("body").find("div#lyricscontent");
 	if(lyrics.length > 0){
@@ -43,50 +40,49 @@ Crawler.prototype.parse = function(body){
 		var musicBy = $("head").find("meta[property='paadalvarigal:music_by']").attr('content');
 		var singers = $("head").find("meta[property='paadalvarigal:singers']").attr('content');		
 		console.log(songName + '-' + movieName);
-		store.addLyrics({"source": this.baseUrl, "song": songName, "movie": movieName, "music": musicBy,"singer": singers,"lyrics": lyrics.html()});		
+		// store.addLyrics({"source": this.baseUrl, "song": songName, "movie": movieName, "music": musicBy,"singer": singers,"lyrics": lyrics.html()});		
 	}
 }
 
-Crawler.prototype.resolveUrl = function(url){	
-	if(url.indexOf(this.baseUrl) != -1){
+function resolveUrl (url){	
+	if(url.indexOf(baseUrl) != -1){
 		return url;
 	} else if(url.indexOf("http://") != -1 || url.indexOf("https://") != -1) {
 		return '';
-	} else if(this.alreadyCrawled(url)){
+	} else if(alreadyCrawled(url)){
 		return '';
 	}else {
-		return  this.baseUrl + url;		
+		return  baseUrl + url;		
 	}
 
 }
 
-Crawler.prototype.alreadyCrawled = function(url){
-	return this.crawledUrls.indexOf(url) > -1;
+function alreadyCrawled(url){
+	return crawledUrls.indexOf(url) > -1;
 }
 
-Crawler.prototype.addToCrawledUrls = function(url){
-	this.crawledUrls.push(url);
+function addToCrawledUrls(url){
+	crawledUrls.push(url);
 }
 
 
-Crawler.prototype.crawl = function (urls){
-	var self = this;	
-	_(urls).forEach(function(url){
-			self.fetch(url, function(body){			 		
-			 		self.parse(body);
-					self.addToCrawledUrls(url);
-					var links = self.filterLinks(body);					
-					emitter.emit('links_found', links);
-			 });			 								
-	});
-			 
+function crawl (url){	
+		fetch(url, function(body){			 		
+			 		parse(body);
+					addToCrawledUrls(url);
+					var links = filterLinks(body);					
+					_(links).forEach(function(link){
+							emitter.emit('link_found', link);
+					});
+		});
+			
 };	
 
-emitter.on('links_found', function(links){	
-	var hd = new memwatch.HeapDiff();
-	new Crawler(seedUrls[0]).crawl(links);
-	console.log(hd.end());
+emitter.on('link_found', function(link){			
+	crawl(link);		
 });
 
 
-new Crawler(seedUrls[0]).crawl(seedUrls);
+
+crawl(seedUrls[0]);
+
