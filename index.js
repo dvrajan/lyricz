84eab5ea0b2a3cb981f03dbb4fababa6f899e122
Deva <dvrajan12@gmail.com>
2014-11-02 21:18:47 +0530

    error handing

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ var seedUrls = ['http://www.paadalvarigal.com'];
 var crawledUrls = [];
 var baseUrl = seedUrls[0];
 
-	
-function filterLinks($){					
+
+function filterLinks($){
 		var links = [];
 		$("body").find("a").each(function(i, element){
 			links[i] = $(this).attr('href');
@@ -20,16 +20,17 @@ function filterLinks($){
 		return links;
 };
 
-function fetch(url, callback){	  		
-	  var newUrl = resolveUrl(url);	  
-	  if(newUrl == '')
+function fetch(url, callback){
+	  var newUrl = resolveUrl(url);
+	  if(newUrl == ''){
 	  	throw 'invalid url';
-		request.get(newUrl, function(error, response, body){	
-			if(!error && response.statusCode == 200){			
-				callback(body);			
+		}
+		request.get(newUrl, function(error, response, body){
+			if(!error && response.statusCode == 200){
+				callback(body);
 			} else {
 				throw error;
-			}									
+			}
 		});
 };
 
@@ -40,19 +41,19 @@ function parse($, url){
 		var songName = main.find("h1 span[itemprop='name']").text();
 		var movieName = main.find("meta[itemprop='inAlbum']").attr('content');
 		var musicBy = $("head").find("meta[property='paadalvarigal:music_by']").attr('content');
-		var singers = $("head").find("meta[property='paadalvarigal:singers']").attr('content');		
+		var singers = $("head").find("meta[property='paadalvarigal:singers']").attr('content');
 		console.log(songName + '-' + movieName + '-' + url);
-		 store.addLyrics({"source": baseUrl, "song": songName, "movie": movieName, "music": musicBy,"singer": singers,"lyrics": lyrics.html()});		
+		 store.addLyrics({"source": baseUrl, "song": songName, "movie": movieName, "music": musicBy,"singer": singers,"lyrics": lyrics.html()});
 	}
 }
 
-function resolveUrl (url){	
+function resolveUrl (url){
 	if(url.indexOf(baseUrl) != -1){
 		return url;
 	} else if(url.indexOf("http://") != -1 || url.indexOf("https://") != -1) {
 		return '';
 	} else {
-		return  baseUrl + url;		
+		return  baseUrl + url;
 	}
 
 }
@@ -66,26 +67,25 @@ function addToCrawledUrls(url){
 }
 
 
-function crawl (url){	
+function crawl (url){
 	try{
 	       fetch(url, function(body){
-					var $ = cheerio.load(body);			 		
+					var $ = cheerio.load(body);
 			 		parse($, url);
 					addToCrawledUrls(url);
-					var links = filterLinks($);					
-					_(links).forEach(function(link){	
-							console.log('pushing')						
-							queue.push(link);						
-					});					
+					var links = filterLinks($);
+					_(links).forEach(function(link){
+							queue.push(link);
+					});
+					pushed();
 		});
 	} catch (err){
 		console.log("Error for " + url);
-	}
 		if(queue.length > 0){
-			pushed();
+		pushed();
 		}
-		
-};	
+	}
+};
 
 function stripQueryParams(url){
 	var parts = url.split('?');
@@ -93,20 +93,19 @@ function stripQueryParams(url){
 }
 
 function pushed(){
-		var url; 
+		var url;
 		var strippedUrl;
 		do{
 		   url = queue.pop()
 		   if(url == undefined){
 		   	 continue;
 		   }
-			 strippedUrl = stripQueryParams(url); 
+			 strippedUrl = stripQueryParams(url);
 		}while(alreadyCrawled(strippedUrl));
-		emitter.emit('pushed', url)		
+		emitter.emit('pushed', url)
 }
 
 emitter.on('pushed', function(url){
-	console.log('Crawling');
 	crawl(url)
 });
 
